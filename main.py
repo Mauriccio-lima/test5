db78acfb1e002d3a5b65210a7b18efbe90277fe8
Mauriccio-lima <65843064+Mauriccio-lima@users.noreply.github.com>
2023-06-19 16:34:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import telebot
 
 bot = telebot.TeleBot("5839756034:AAHBl1NiRzjmmXyjwcAQmb24HLbJblHgJhE")
 
+@bot.message_handler(commands=['start'])
+def messag(dizer):
+	bot.reply_to(dizer,'foi alterado')
+
 
 @bot.message_handler(content_types=['document', 'audio'])
 def handle_docs_audio(message):
