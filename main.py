197246b037139a4626fb8405082a269c265009ff
Mauriccio-lima <65843064+Mauriccio-lima@users.noreply.github.com>
2023-06-19 12:40:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import telebot
 bot = telebot.TeleBot("5839756034:AAHBl1NiRzjmmXyjwcAQmb24HLbJblHgJhE")
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+def send_welcome(diga):
+	bot.reply_to(diga, "Howdy, how are you doing?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
