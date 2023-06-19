5940db19e25effa33553aaf317262c35b5ab1e21
Mauriccio-lima <65843064+Mauriccio-lima@users.noreply.github.com>
2023-06-19 14:11:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import telebot
 
 bot = telebot.TeleBot("5839756034:AAHBl1NiRzjmmXyjwcAQmb24HLbJblHgJhE")
 
-@bot.message_handler(commands=['start'])
-def send_wel(diga):
-	bot.reply_to(diga,"foi alterado5")
 
-send_wel(diga,"ok")
-
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_docs_audio(message):
+	pass
 
 bot.infinity_polling()
