08c978f7b5b487d8a18f8bdc9bdc82847e510448
Mauriccio-lima <65843064+Mauriccio-lima@users.noreply.github.com>
2023-06-19 12:44:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ bot = telebot.TeleBot("5839756034:AAHBl1NiRzjmmXyjwcAQmb24HLbJblHgJhE")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(diga):
-	bot.reply_to(diga, "Howdy, how are you doing?")
+	bot.reply_to(diga, "foi alterado")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
-bot.polling()
+bot.infinity_polling()
