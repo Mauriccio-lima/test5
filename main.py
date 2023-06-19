9ff49527bd16fb74fff2ccf097beaf7e4ff1e761
Mauriccio-lima <65843064+Mauriccio-lima@users.noreply.github.com>
2023-06-19 13:02:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot("5839756034:AAHBl1NiRzjmmXyjwcAQmb24HLbJblHgJhE")
 
 @bot.message_handler(commands=['start'])
 def send_wel(diga):
-	bot.reply_to(diga, "foi alterado3")
-	bot.reply_to(diga, "belezinhaa")
+	bot.reply_to("foi alterado4")
+
 
 bot.infinity_polling()
