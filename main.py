018717b9283b75446a89ebad22a3842e2e4ef7da
Mauriccio-lima <65843064+Mauriccio-lima@users.noreply.github.com>
2023-06-19 16:30:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot("5839756034:AAHBl1NiRzjmmXyjwcAQmb24HLbJblHgJhE")
 
 @bot.message_handler(content_types=['document', 'audio'])
 def handle_docs_audio(message):
-	pass
+	bot.reply_to(message, 'ok')
 
 bot.infinity_polling()
